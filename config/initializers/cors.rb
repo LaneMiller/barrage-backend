fbaf13477b053d64c9338028eb87a7c54d113a26
Lane Miller <chris.lane.miller@gmail.com>
2019-02-03 12:18:50 -0500

    updated origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'barrage-arcade.herokuapp.com/'
 
     resource '*',
       headers: :any,
