b8dd03fcdf1ea6143f83f633c99db80e1818a8ba
Lane Miller <chris.lane.miller@gmail.com>
2019-02-03 12:13:28 -0500

    added requirement to rack/cors gem

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['https://barrage-arcade.herokuapp.com/']
+    origins 'https://barrage-arcade.herokuapp.com/'
 
     resource '*',
       headers: :any,
